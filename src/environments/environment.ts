2f8b1eb83ab984386527fc59901801286f9aaa16
trilgar <zaritkiy.alexey@gmail.com>
2021-05-10 00:27:55 +0300

    1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   // backend_url: 'http://localhost:8080/'
   backend_url: 'https://stddb.herokuapp.com'
 };
