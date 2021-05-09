e2dd95cb810c38ba5bc96068b162acba2c580b43
trilgar <zaritkiy.alexey@gmail.com>
2021-05-10 00:49:41 +0300

    revert fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // backend_url: 'http://localhost:8080/'
-  backend_url: 'https://stddb.herokuapp.com'
+  backend_url: 'https://stddb.herokuapp.com/'
 };
