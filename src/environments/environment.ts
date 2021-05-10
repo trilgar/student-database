64ce89f78881b6792a34ad66cbedfbc3c9161909
trilgar <zaritkiy.alexey@gmail.com>
2021-05-10 17:54:08 +0300

    url fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  // backend_url: 'http://localhost:8080/'
-  backend_url: 'https://stddb.herokuapp.com'
+  backend_url: 'http://localhost:8080/'
+  // backend_url: 'https://stddb.herokuapp.com'
 };
 
 /*
